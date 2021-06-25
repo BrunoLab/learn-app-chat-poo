c6a5ed3a960bee96cb19a159dee839f286f04498
Bruno SCHOLTES <b.scholtes@free.fr>
2021-06-25 17:25:55 +0200

    customize Post model

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -2,8 +2,27 @@
 
 namespace App\Models;
 
+use DateTime;
+
 class Post extends Model{
 
     protected $table = 'posts';
+
+    public function getCreatedAt(): string
+    {
+        return (new DateTime($this->created_at))->format('d/m/Y à h:m');
+    }
+
+    public function getExcerpt(): string
+    {
+        return substr($this->content, 0, 200) . '...';
+    }
+
+    public function getButton()
+    {
+        return <<<HTML
+        <a href="/posts/$this->id" class="btn btn-primary">Lire plus</a>
+HTML;
+    }
 
 }
