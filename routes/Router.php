9c3aee67408222b3c3caf3dc0b6561cc9d896ddf
Bruno SCHOLTES <b.scholtes@free.fr>
2021-06-30 13:55:54 +0200

    Customize Exceptions with views

--- routes/Router.php
+++ routes/Router.php
@@ -2,6 +2,8 @@
 
 namespace Router;
 
+use App\Exceptions\NotFoundException;
+
 class Router{
 
     public $url;
@@ -26,7 +28,7 @@ class Router{
             }
         }
 
-        return header('HTTP/1.0 404 Not Found');
+        throw new NotFoundException('La page demandée est introuvable');
     }
 
 }
