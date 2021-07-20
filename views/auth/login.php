3fe777c95a0f1e529580b44810bae205fc4c4924
Bruno SCHOLTES <b.scholtes@free.fr>
2021-07-20 09:36:21 +0200

    form post data validation

--- views/auth/login.php
+++ views/auth/login.php
@@ -1,3 +1,16 @@
+<?php if (isset($_SESSION['errors'])): ?>
+    <?php foreach($_SESSION['errors'] as $errorsArray): ?>
+        <?php foreach($errorsArray as $errors): ?>
+            <div class="alert alert-danger">
+                <?php foreach($errors as $error): ?>
+                    <li><?= $error ?></li>
+                <?php endforeach ?>
+            </div>
+        <?php endforeach ?>
+    <?php endforeach ?>
+<?php endif ?>
+<?php session_destroy(); ?>
+
 <h1>Se connecter</h1>
 
 <form action="/login" method="POST">
